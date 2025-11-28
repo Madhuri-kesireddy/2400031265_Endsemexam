b96c654e3e954c1177f2d621019d19d087485de8
2400031265 <2400031265@kluniversity.in>
2025-11-28 20:21:09 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import StudentDetails from "./StudentDetails";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Student Portal</h1>
+      <StudentDetails />
+    </div>
+  );
+}
